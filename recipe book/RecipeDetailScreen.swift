8c925334a8b78f6a68feccd92d48ed4d0e134df0
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-06-27 14:54:47 -0500

    progress

--- recipe book/RecipeDetailScreen.swift
+++ recipe book/RecipeDetailScreen.swift
@@ -14,7 +14,11 @@ class RecipeDetailScreen: UIViewController {
         super.viewDidLoad()
 
         title = recipe.name // the view title
+
         descriptionLabel.text = recipe.description
+
+        // This uses a single UILabelView to display all the ingredients
+        // by separating them with newline characters.
         ingredientsLabel.text = recipe.ingredients.map { "• \($0)" }
             .joined(separator: "\n")
     }
