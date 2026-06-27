53beabf2cd0d1d77859200834bc0a5f9fa0c8e3c
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-06-27 14:43:30 -0500

    progress

--- recipe book/RecipeDetailScreen.swift
+++ recipe book/RecipeDetailScreen.swift
@@ -4,7 +4,7 @@ class RecipeDetailScreen: UIViewController {
     @IBOutlet private var descriptionLabel: UILabel!
     @IBOutlet private var ingredientsLabel: UILabel!
 
-    private var recipe: Recipe?
+    private var recipe: Recipe!
 
     func configure(with recipe: Recipe) {
         self.recipe = recipe
@@ -12,8 +12,6 @@ class RecipeDetailScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        guard let recipe else { return }
 
         title = recipe.name
         descriptionLabel.text = recipe.description
