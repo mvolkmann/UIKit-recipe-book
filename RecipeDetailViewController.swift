4c1e50b5032a65affa3e77a4d9b56777943663e1
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-06-27 14:16:54 -0500

    progress

--- RecipeDetailViewController.swift
+++ RecipeDetailViewController.swift
@@ -5,26 +5,26 @@ class RecipeDetailViewController: UIViewController {
 
     init(recipe: Recipe) {
         self.recipe = recipe
+
+        // We need to run the init method in UIViewController even though
+        // this screen is built in code instead of loaded from a nib file.
+        // A nib file is a compiled Interface builder file.
         super.init(nibName: nil, bundle: nil)
     }
 
     // Because the superclass UIViewController has a required init?(coder:),
-    // this subclass must defined it.
+    // this subclass must also defined it.  But it will never be called.
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // This programmatically creates the view.
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = recipe.name
         view.backgroundColor = .systemBackground
-
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 16
-        stackView.translatesAutoresizingMaskIntoConstraints = false
 
         let descriptionLabel = UILabel()
         descriptionLabel.text = recipe.description
@@ -41,20 +41,26 @@ class RecipeDetailViewController: UIViewController {
         ingredientsLabel.font = .preferredFont(forTextStyle: .body)
         ingredientsLabel.numberOfLines = 0
 
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        // Disable autoresizing mask constraints
+        // so Auto Layout uses the constraints below.
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
         stackView.addArrangedSubview(descriptionLabel)
         stackView.addArrangedSubview(ingredientsTitleLabel)
         stackView.addArrangedSubview(ingredientsLabel)
         view.addSubview(stackView)
 
+        let topSpace = view.safeAreaLayoutGuide.topAnchor
+        let leadingSpace = view.layoutMarginsGuide.leadingAnchor
+        let trailingSpace = view.layoutMarginsGuide.trailingAnchor
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor,
-                constant: 24
-            ),
-            stackView.leadingAnchor
-                .constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            stackView.trailingAnchor
-                .constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+            // Begin 24 points below the top of the safe area.
+            stackView.topAnchor.constraint(equalTo: topSpace, constant: 24),
+            stackView.leadingAnchor.constraint(equalTo: leadingSpace),
+            stackView.trailingAnchor.constraint(equalTo: trailingSpace)
         ])
     }
 }
